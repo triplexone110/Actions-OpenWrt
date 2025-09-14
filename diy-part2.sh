0f1a7bde8f91eb754c8adaafb0daef81b1cc01c3
triplexone110 <62888096+triplexone110@users.noreply.github.com>
2025-09-14 18:23:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Install LuCI dependencies
+./scripts/feeds install luci luci-i18n-base-zh-cn
 # Modify default IP and other settings
 sed -i 's/192.168.1.1/192.168.6.2/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/ImmortalWrt-Pi4/g' package/base-files/files/bin/config_generate
