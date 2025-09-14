72b52d833ead3e780d194a7760c1b55ae54faae3
triplexone110 <62888096+triplexone110@users.noreply.github.com>
2025-09-14 16:13:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more details.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# 1. 修改默认IP地址、主机名、时区等
+# Modify default IP and other settings
 sed -i 's/192.168.1.1/192.168.6.2/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/ImmortalWrt-Pi4/g' package/base-files/files/bin/config_generate
 sed -i "s/'UTC'/'CST-8'/g" package/base-files/files/bin/config_generate
-
-# 2. 添加 SSR-Plus 软件源 (因为 .config 里选了它, 所以必须添加它的源码)
-git clone https://github.com/fw876/helloworld.git package/helloworld
-
-echo "DIY script part 2 finished."
