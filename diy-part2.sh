cdc7f93f94b2228fce5bd1c955bb0a5788b51aa1
triplexone110 <62888096+triplexone110@users.noreply.github.com>
2025-09-14 22:18:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# Install LuCI dependencies with force option
+# Remove default-settings if enabled
+sed -i '/CONFIG_PACKAGE_default-settings/d' .config
+sed -i '/CONFIG_PACKAGE_default-settings-chn/d' .config
+echo "# CONFIG_PACKAGE_default-settings is not set" >> .config
+echo "# CONFIG_PACKAGE_default-settings-chn is not set" >> .config
+# Install LuCI dependencies
 ./scripts/feeds install -f luci luci-i18n-base-zh-cn
 # Modify default IP and other settings
 sed -i 's/192.168.1.1/192.168.6.2/g' package/base-files/files/bin/config_generate
