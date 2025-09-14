7b08b931660f003ab64b618e9b08e7b4e74869b1
triplexone110 <62888096+triplexone110@users.noreply.github.com>
2025-09-14 18:22:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo 'src-git luci https://github.com/immortalwrt/luci.git;openwrt-23.05' >> feeds.conf.default
+echo 'src-git luci https://github.com/openwrt/luci.git;openwrt-24.10' >> feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
